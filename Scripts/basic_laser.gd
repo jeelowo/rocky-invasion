8e745c830cab606f5a8af1f39e5ca9f4bd95c1b5
jeelowo <plegariajamuel47@gmail.com>
2026-08-05 00:36:46 +0800

    alien can now take damage

--- Scripts/basic_laser.gd
+++ Scripts/basic_laser.gd
@@ -8,3 +8,9 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	global_position += transform.x * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy") and body.has_method("take_damage"):
+		body.take_damage(2)
+	else:
+		print("no mewthod")
