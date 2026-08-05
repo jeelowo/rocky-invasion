177186800fdb03f7d749cc941023beff45865612
jeelowo <plegariajamuel47@gmail.com>
2026-08-05 23:11:17 +0800

    added alien spawner

--- Scripts/basic_laser.gd
+++ Scripts/basic_laser.gd
@@ -12,5 +12,6 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy") and body.has_method("take_damage"):
 		body.take_damage(2)
+		self.queue_free()
 	else:
 		print("no mewthod")
